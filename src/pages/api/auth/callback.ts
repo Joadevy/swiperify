655c181d1d5a99da4b66de0698fc2ee320d8cfa9
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-01-28 18:12:54 -0300

    chore: code ideas

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -16,6 +16,17 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
 
   const { access_token, refresh_token } = data.session;
 
+  // Pareciera que aca deberia guardarse el token de spotify para poder hacer las llamadas a la api de spotify, podria
+  // guardarse el token en una cookie y luego leerlo desde el cliente para hacer las llamadas a la api de spotify
+  // Authorization code flow
+// Spotify supports OAuth 2.0 for authenticating all API requests.
+
+// Authorization url
+// https://accounts.spotify.com/authorize
+
+// Token url
+// https://accounts.spotify.com/api/token
+
   cookies.set("sb-access-token", access_token, {
     path: "/",
   });
