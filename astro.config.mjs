a141fbee2368a0dfd3d62a6bc286a50d5846cbcb
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-02-06 19:15:54 -0300

    chore: migrate to react from preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
-import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel()
 });
