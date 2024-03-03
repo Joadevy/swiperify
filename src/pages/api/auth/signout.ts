be507b67aec5dac76f219f4aa3670e8c52a6c461
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-03-03 18:47:17 -0300

    chore: remove cookies when signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -2,13 +2,19 @@ import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
-  const { error } = await supabase.auth.signOut({scope:'others'})
+  const { error } = await supabase.auth.signOut({
+    scope:"global"
+  });
 
 if (error) {
     return new Response(error.message, { status: 500 });
 }
 
-  cookies.delete("sb-access-token", { path: "/" });
-  cookies.delete("sb-refresh-token", { path: "/" });
+  cookies.delete("sb-access-token", {path: '/'});
+  cookies.delete("sb-refresh-token", {path: '/'});
+  cookies.delete('spotify-access-token', {path: '/'});
+  cookies.delete('spotify-refresh-token', {path: '/'});
+
+
   return redirect("/");
 };
