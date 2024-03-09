979f7e9cb7694298fcc54b95e48e7c837632995c
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-03-09 18:05:13 -0300

    feat: add modal window to log out from spotify

---
+++ src/components/Logout.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+function LogoutModal() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+   useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isOpen]);
+
+  const logout = () => {
+    window.open('https://www.spotify.com/logout/', '_blank');
+    closeModal();
+    window.location.replace('/');
+  };
+
+  return (
+    <div className={isOpen ? 'fixed top-0 left-0 w-full h-full bg-black bg-opacity-50' : ''}>
+      {isOpen && (
+        <dialog
+          open
+          className="bg-zinc-800 text-slate-200 shadow-lg rounded-md w-80 sm:w-[500px] inset-0"
+        >
+          <div className="p-4 rounded m-4">
+            <h2 className="text-xl font-bold mb-4">Logout process information</h2>
+            <p className='text-base'>
+              If you <span className='text-accent-light'> want to log in with another Spotify account the next time </span>
+               you'll need to logout your actual Spotify session.
+            </p>
+            <p className="mt-2 mb-4 text-base">
+              We can do this for you if you <span className=' text-accent-light'>
+                click the procced button</span> and then please close the tab
+              to continue using this app.
+            </p>
+
+            <footer className='flex text-base justify-between'>
+              <button onClick={logout} className="px-2 py-1 bg-accent-light text-zinc-800 rounded-sm hover:scale-105 hover:cursor-pointer transition-all hover:opacity-85'">
+                Proceed
+              </button>
+              <button onClick={closeModal} className="bg-zinc-700 py-1 px-2 rounded-sm hover:opacity-75 transition-opacity">
+                Remain my session
+              </button>
+            </footer>
+          </div>
+        </dialog>
+      )}
+    </div>
+  );
+}
+
+export default LogoutModal;
