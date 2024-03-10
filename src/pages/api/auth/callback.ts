5cf3d62ebea674f8826737919bec6d5da3c56dd3
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-03-10 18:11:55 -0300

    feat: add spotify plan control & show in discover page

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -16,6 +16,29 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
 
   const { access_token, refresh_token, provider_token, provider_refresh_token } = data.session;
 
+
+  const fetchOptions = {
+  headers: {
+    'Authorization': `Bearer ${provider_token}`
+  }
+};
+
+// Obtendriamos simplemente la info de si es premium o no
+  const isPremiumUser :Promise<{
+    isPremium:boolean,
+    plan:string
+  }> = fetch(`https://api.spotify.com/v1/me`, fetchOptions)
+    .then(response => response.json())
+    .then(userData => {
+      return {isPremium: userData.product === 'premium', plan: userData.product};
+  });
+
+  const {isPremium} = await isPremiumUser;
+
+  cookies.set("is-premium", isPremium.toString(), {
+    path: "/",
+  });
+
   // Store the Spotify access token in a cookie
   cookies.set("spotify-access-token", provider_token!, {
     path: "/",
